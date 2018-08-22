2c0f61ad36936f4dce3ea58013cf79fbd8d008ce
TrevorSeitz <trevinla@gmail.com>
2018-08-22 10:08:36 -0400

    change disply of new dog info

--- app/assets/javascripts/walks.js
+++ app/assets/javascripts/walks.js
@@ -34,11 +34,13 @@ function attachListeners() {
       // $("div.returned_new_dog").append(`<li>${data}</li>`);
       $("div.returned_new_dog").append(
         `<ul>
-        Congratulations on your new Dog ${data.name}
+        <h3>Congratulations on your new Dog ${data.name}!<h3>
         <li>Breed: ${data.breed}</li>
         <li>Age: ${data.age}</li>
         <li>Notes: ${data.notes}</li>
-        </ul>`
+        <p></p>
+        </ul>
+        <p>Do you have any more new dogs?</p>`
       );
     });
   });
