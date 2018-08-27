25cce2902c088de6e2dc66129429ff8d57b5d260
TrevorSeitz <trevinla@gmail.com>
2018-08-27 08:38:56 -0400

    new dogs asset

---
+++ app/assets/javascripts/dogs.js
@@ -0,0 +1,17 @@
+$(".new_dog").on("submit", function(e) {
+  e.preventDefault();
+  $.ajax({
+    method: "POST",
+    url: this.action,
+    data: $(".new_dog :input")
+  }).done(function(data) {
+    $("div.returned_new_dog").append(
+      `<ul>
+      Congratulations on your new Dog ${data.name}
+      <li>Breed: ${data.breed}</li>
+      <li>Age: ${data.age}</li>
+      <li>Notes: ${data.notes}</li>
+      </ul>`
+    );
+  });
+});
